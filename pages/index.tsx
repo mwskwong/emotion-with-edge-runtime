9a71bab016cb8a4ca859f1d2535d0dfff16915a7
Matthew Kwong <matthewkwong2@gmail.com>
2022-10-29 09:16:26 +0800

    feat: first commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div
+      css={{
+        padding: 32,
+        backgroundColor: "hotpink",
+        fontSize: 24,
+        borderRadius: 4
+      }}
+    />
+  )
+}
